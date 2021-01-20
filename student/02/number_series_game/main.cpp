9b7633fedc438bc7233aafed4942c2babb4a90f0
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-01-20 19:46:01 +0200

    Lisätty myös 7:llä jaollisten lukujen käsittely

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -8,7 +8,13 @@ int main()
 
     int i = 1;
     while ( i <= numero ) {
-        if ( i % 3 == 0 ) {
+        if ( i % 3 == 0 and i % 7 == 0) {
+            std::cout << "zip boing" << std::endl;
+        }
+        else if ( i % 7 == 0) {
+            std::cout << "boing" << std::endl;
+        }
+        else if ( i % 3 == 0 ) {
              std::cout << "zip" << std::endl;
         }
         else {
