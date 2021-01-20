0f01e7cb5d702c07f1397f1ea45a2afcccbecca0
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-01-20 19:08:27 +0200

    Syötteen lukeminen ja numeroiden tulostus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -3,5 +3,13 @@
 
 int main()
 {
+    int numero = 0;
     std::cout << "How many numbers would you like to have? ";
+    std::cin >> numero;
+
+    int i = 1;
+    while ( i <= numero ) {
+        std::cout << i << std::endl;
+        ++i;
+    }
 }
