0f98294cf543617e260f2de744e1e1d424591790
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-01-22 18:03:05 +0200

    Valmis ohjelma

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>    /* sqrt */
 
 using namespace std;
 
@@ -11,8 +12,23 @@ int main()
         if ( number <= 0) {
             cout << "Only positive numbers accepted" << endl;
         }
-
-
     }
+
+    double number_rooted = sqrt(number);
+    int number_rounded = ceil(number_rooted);
+
+    int index = number_rounded;
+    while (index > 0 ) {
+
+        if (number % index == 0) {
+            int number2 = 0;
+            number2 = number / index;
+            cout << number << " = " << index << " * " << number2 << endl;
+            break;
+        }
+    --index;
+    }
+
+
     return 0;
 }
