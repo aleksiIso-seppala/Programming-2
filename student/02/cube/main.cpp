00cdfc09f5327e5d804f345a119b1056ee9c7359
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-01-22 17:12:59 +0200

    kuutio-ohjelma valmis

---
+++ student/02/cube/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int number = 0;
+    cout << "Enter a number: ";
+    cin >> number;
+    int number_squared = number * number * number;
+
+    if ( number_squared < 0 or number_squared <= number) {
+        cout << "Error! The cube of " << number << " is not " << number_squared << "." << endl;
+    }
+    else {
+      cout << "The cube of " << number << " is " << number_squared << "." << endl;
+    }
+    return 0;
+}
