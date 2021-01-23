2bb108ec813bf49f6b6b62b152988d8a46de2a7f
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-01-23 17:42:35 +0200

    main funktio valmiina

---
+++ student/02/lotto/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    unsigned long int total_balls = 0;
+    cout << "Enter the total number of lottery balls: " << endl;
+    cin >> total_balls;
+
+    unsigned long int drawn_balls = 0;
+    cout << "Enter the number of drawn balls: " << endl;
+    cin >> drawn_balls;
+
+    return 0;
+}
