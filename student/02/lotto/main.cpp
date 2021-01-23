6d1e42da67af6b99b4ed5e8e2626aaf2fe873d91
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-01-23 17:49:36 +0200

    suoritettu virhetarkastelu

--- student/02/lotto/main.cpp
+++ student/02/lotto/main.cpp
@@ -14,3 +14,16 @@ int main()
 
     return 0;
 }
+
+int check_number (unsigned long int total_balls, unsigned long int drawn_balls) {
+
+    if (total_balls <= 0 or drawn_balls <= 0) {
+        cout << "The number of balls must be a positive number." << endl;
+    }
+    else if (total_balls < drawn_balls) {
+        cout << "The maximum number of drawn balls is the total amount of balls" << endl;
+    }
+    else {
+        return 1;
+    }
+}
