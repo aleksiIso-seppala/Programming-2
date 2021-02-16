27c034f07a447c5bfcd03b1f531bcdff9425fac5
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-02-16 15:36:06 +0200

    Valmis ohjelma

---
+++ student/05/line_numbers/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string input_file = "";
+    cout << "Input file: ";
+    cin >> input_file;
+
+    string output_file = "";
+    cout << "Output file; ";
+    cin >> output_file;
+
+    ifstream tiedosto_olio(input_file);
+    ofstream tiedost_olio_2(output_file);
+    if (not tiedosto_olio) {
+        cout << "Error! The file " << input_file << " cannot be opened." << endl;
+    }
+    else if (not tiedost_olio_2) {
+       cout << "Error! The file " << output_file << " cannot be opened." << endl;
+    }
+    else {
+        int index = 1;
+        string rivi;
+        string rivi2;
+        while ( getline(tiedosto_olio, rivi)) {
+            string line = rivi;
+            tiedost_olio_2 << index << " " << line << endl;
+        }
+        tiedosto_olio.close();
+        tiedost_olio_2.close();
+    }
+    return 0;
+}
