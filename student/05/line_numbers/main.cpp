f5adb507b3bd2341173670fb0fd97563f2030b6d
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-02-17 14:10:48 +0200

    ongelma oletettavasti korjattu

--- student/05/line_numbers/main.cpp
+++ student/05/line_numbers/main.cpp
@@ -15,19 +15,18 @@ int main()
     getline(cin, output_file);
 
     ifstream tiedosto_olio(input_file);
-    ofstream tiedost_olio_2(output_file);
     if (not tiedosto_olio) {
         cout << "Error! The file " << input_file << " cannot be opened." << endl;
         return 1;
     }
-    else if (not tiedost_olio_2) {
+    ofstream tiedost_olio_2(output_file);
+    if (not tiedost_olio_2) {
        cout << "Error! The file " << output_file << " cannot be opened." << endl;
        return 1;
     }
     else {
         int index = 1;
         string rivi;
-        string rivi2;
         while ( getline(tiedosto_olio, rivi)) {
             string line = rivi;
             tiedost_olio_2 << index << " " << line << endl;
