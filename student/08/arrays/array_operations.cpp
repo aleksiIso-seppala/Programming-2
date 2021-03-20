5c6e3ee97a06a35e13b45958a2f58aaeb4ff933e
Aleksi Iso-Seppälä <aleksi.iso-seppala@tuni.fi>
2021-03-20 15:41:22 +0200

    valmis Ohjelma

--- student/08/arrays/array_operations.cpp
+++ student/08/arrays/array_operations.cpp
@@ -1,1 +1,52 @@
 #include "array_operations.hh"
+
+int greatest_v1(int* itemptr, int size){
+
+    int* pointer = nullptr;
+    pointer = itemptr;
+    int largest_number = 0;
+    while(pointer < itemptr+size){
+        if (largest_number < *pointer){
+            largest_number = *pointer;
+        }
+        ++pointer;
+        continue;
+    }
+    return largest_number;
+}
+
+int greatest_v2(int* itemptr, int* endptr){
+
+    int* pointer = nullptr;
+    pointer = itemptr;
+    int largest_number = 0;
+    while(pointer < endptr){
+        if (largest_number < *pointer){
+            largest_number = *pointer;
+        }
+        ++pointer;
+        continue;
+    }
+    return largest_number;
+}
+
+void copy(int* itemptr, int* endptr, int* targetptr){
+
+    for(; itemptr != endptr; ++itemptr, ++targetptr){
+
+        *targetptr = *itemptr;
+        continue;
+    }
+    return;
+
+}
+
+void reverse(int* leftptr, int* rightptr){
+
+    for(--rightptr; leftptr < rightptr; ++leftptr, --rightptr){
+
+        int tmp = *leftptr;
+        *leftptr = *rightptr;
+        *rightptr = tmp;
+    }
+}
